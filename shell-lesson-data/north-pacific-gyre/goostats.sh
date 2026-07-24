7f71c428647cbb4251bd0f2ac9837a42a10c35f8
John Duggan <dugganjw@ornl.gov>
2026-07-23 20:58:19 -0400

    Add details to file error messages

--- shell-lesson-data/north-pacific-gyre/goostats.sh
+++ shell-lesson-data/north-pacific-gyre/goostats.sh
@@ -13,11 +13,11 @@ fi
 # check if files already exist (good for $1, bad for $2)
 if [ ! -e "$1" ]
 then
-    echo "error reading input: $1" >&2
+    echo "error reading input: file $1 does not exist" >&2
     exit 2
 elif [ -e "$2" ]
 then
-    echo "error writing result: $2" >&2
+    echo "error writing result: file $2 already exists" >&2
     exit 2
 fi
 
